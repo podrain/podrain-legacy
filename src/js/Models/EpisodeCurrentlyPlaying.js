e7197ddd0ac69d1b422bb7ca8996320ac1f2f6c8
Joe Sweeney <joe.sweeney224@gmail.com>
2020-02-25 15:44:52 -0700

    Add some comments for clarity

--- src/js/Models/EpisodeCurrentlyPlaying.js
+++ src/js/Models/EpisodeCurrentlyPlaying.js
@@ -14,47 +14,51 @@ let EpisodeCurrentlyPlaying = {
       this.audio = null
     }
 
+    // Get currently playing episode
     let currentlyPlayingEpisode = await State.db.find({
       selector: {
         currently_playing: true
       }
     })
 
+    // Set episode to not playing anymore
     await State.db.bulkDocs(currentlyPlayingEpisode.docs.map(doc => {
       doc.currently_playing = false
       return doc
     }))
 
+    // Queue up new episode
     let episodeToPlay = await State.db.find({
       selector: {
         _id: id,
         type: 'episode'
       }
     })
-
     this.episode = episodeToPlay.docs[0]
 
+    // Get episode podcast data side-loaded
     let episodePodcast = await State.db.find({
       selector: {
         _id: this.episode.podcast_id,
         type: 'podcast'
       }
     })
-        
     this.episode.podcast = episodePodcast.docs[0]
 
+    // Update currently playing to the new episode
     let currentlyPlayingEpisodeUpdate = _.merge(this.episode, {
       currently_playing: true
     })
-
     await State.db.put(currentlyPlayingEpisodeUpdate)
 
+    // Start playing the episode
     this.audio = new Audio
     this.audio.src = this.episode.enclosure.url
     this.audio.currentTime = this.episode.playhead
     this.audio.load()
     this.audio.play()
 
+    // Refresh the queue
     await QueueModel.getQueue()
   },
 
