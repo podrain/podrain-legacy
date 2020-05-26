f44b3cf17136c4b7eb9f19c1efb66d49d67ec7a0
Joe Sweeney <joe.sweeney224@gmail.com>
2020-05-25 18:07:20 -0600

    Make episode search a little nicer

--- src/js/Components/EpisodeSearch.js
+++ src/js/Components/EpisodeSearch.js
@@ -7,20 +7,23 @@ function EpisodeSearch() {
     view() {
       return EpisodeSearchModel.loading 
       ? m('.flex.text-white.text-5xl.h-full.justify-center.items-center', 'Loading...')
-      : [
-        m('h1.text-white', 'Search '+EpisodeSearchModel.podcast.meta.title),
-        m('input.p-3.w-full', {
-          oninput(e) {
-            EpisodeSearchModel.searchEpisodes(e.target.value)
-          }
-        }),
-        m('ul.text-white.m-3', EpisodeSearchModel.searchResults.map((ep) => {
-          return m(Episode, {
-            episode: ep,
-            partOf: EpisodeSearchModel.searchResults
-          })
-        })),
-      ]
+      : m('.p-3', [
+          m('h1.text-white', [
+            'Search episodes in ',
+            m('span.italic', EpisodeSearchModel.podcast.meta.title)
+          ]),
+          m('input.p-1.w-full.mt-1', {
+            oninput(e) {
+              EpisodeSearchModel.searchEpisodes(e.target.value)
+            }
+          }),
+          m('ul.text-white.m-3', EpisodeSearchModel.searchResults.map((ep) => {
+            return m(Episode, {
+              episode: ep,
+              partOf: EpisodeSearchModel.searchResults
+            })
+          })),
+        ])
     }
   }
 }
