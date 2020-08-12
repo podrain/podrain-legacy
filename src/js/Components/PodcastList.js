88c511939fb0ed80d9d63aff5ede1db4aef6ada1
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-12 14:11:31 -0600

    encourage users to add podcasts

--- src/js/Components/PodcastList.js
+++ src/js/Components/PodcastList.js
@@ -23,6 +23,7 @@ function PodcastList() {
       return loading
       ? m('.flex.text-white.text-5xl.h-full.justify-center.items-center', 'Loading...')
       : [
+        podcasts.length > 0 ?
         m('.flex.flex-wrap', podcasts.map((pc) => {
           return m('.w-1/3.flex-none', [
             m('img', {
@@ -32,7 +33,14 @@ function PodcastList() {
               }
             })
           ])
-        }))
+        })) : m('.flex.flex-col.h-full.justify-center.items-center', [
+          m('h2.text-white', 'No podcasts.'),
+          m('button.bg-green-500.mt-3.p-2.text-white', {
+            onclick() {
+              m.route.set('/podcasts/add')
+            }
+          }, 'Add some!')
+        ])
       ]
     }
   }
