92d7e0c13c5ce4a2c559c96c3fdd1144607830be
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-12 10:05:20 -0600

    git rid of PodcastCreateModel

--- src/js/Components/PodcastCreate.js
+++ src/js/Components/PodcastCreate.js
@@ -1,8 +1,93 @@
 import m from 'mithril'
-import PodcastCreateModel from '../Models/PodcastCreateModel'
+import State from '../State'
+import feedParser from 'better-podcast-parser'
+import uuidv4 from 'uuid/v4'
+import _ from 'lodash'
 
 function PodcastCreate() {
   let selectedTab = 'search'
+
+  let feedUrl = ''
+  let manualRssUrl = ''
+  let search = ''
+  let searchResults = []
+  let searching = false
+  let episodesAdded = 0
+  let episodesTotal = 0
+
+  function setSearch(value) {
+    search = value
+    searching = true
+    searchPodcastsDelay()
+  }
+
+  function addPodcast(podcastUrl) {
+    feedUrl = podcastUrl
+    return m.request(localStorage.getItem('proxy_url') + feedUrl, {
+      extract: function(xhr) {
+        return xhr
+      },
+      headers: {
+        'accept': 'application/rss+xml, application/rdf+xml;q=0.8, application/atom+xml;q=0.6, application/xml;q=0.4, text/xml;q=0.4'
+      }
+    }).then(response => {
+      let feedData = response.responseText
+      return feedParser.parseFeed(feedData, {
+        proxyURL: localStorage.getItem('proxy_url'),
+        getAllPages: true
+      })
+    }).then(podcast => {
+      let podcastOnly = _.clone(podcast)
+
+      delete podcastOnly.episodes
+
+      let podcastID = uuidv4()
+      episodesTotal = podcast.episodes.length
+
+      let addPodcast = State.dexieDB.podcasts.add(_.merge(podcastOnly, {
+        '_id': podcastID,
+        'feed_url': feedUrl
+      }))
+
+      let addPodcastEpisodes = []
+      for (let ep of podcast.episodes) {
+        addPodcastEpisodes.push(State.dexieDB.episodes.add(_.merge(ep, {
+          '_id': uuidv4(),
+          'podcast_id': podcastID,
+          'queue': 0,
+          'playhead': 0,
+          'currently_playing': false,
+          'played': false
+        })).then(() => {
+          episodesAdded += 1
+          m.redraw()
+        }))
+      }
+
+      return Promise.all([addPodcast, ...addPodcastEpisodes])
+    }).then(() => {
+      feedUrl = ''
+      episodesAdded = 0
+      episodesTotal = 0
+    })
+  }
+
+  let searchPodcastsDelay = _.debounce(() => {
+    searchPodcasts()
+  }, 250)
+
+  function searchPodcasts() {
+    return m.request('https://itunes.apple.com/search', {
+      params: {
+        term: search,
+        media: 'podcast',
+        entity: 'podcast'
+      }
+    }).then(result => {
+      searchResults = result.results
+      searching = false
+    })
+  }
 
   return {
     view() {
@@ -34,13 +119,13 @@ function PodcastCreate() {
               type: 'text',
               placeholder: 'Podcast title to search...',
               oninput(e) {
-                PodcastCreateModel.setSearch(e.target.value)
+                setSearch(e.target.value)
               }
             }),
-            PodcastCreateModel.searching 
+            searching 
             ? m('h2.mt-3.text-white', 'Searching...')
             : m('ul.mt-3', [
-              PodcastCreateModel.searchResults.map((sr, index) => {
+              searchResults.map((sr, index) => {
                 return m('li.flex.text-white', {
                   class: index > 0 ? 'mt-3' : '',
                 }, [
@@ -50,30 +135,30 @@ function PodcastCreate() {
                     }),
                     m('.w-3/4.ml-3', [
                       m('h3', sr.collectionName),
-                      PodcastCreateModel.feedUrl == sr.feedUrl
+                      feedUrl == sr.feedUrl
                       ? [
                         m('.italic.text-gray-200', [
                           'Adding episodes: ',
                           m('br'),
-                          PodcastCreateModel.episodesAdded
+                          episodesAdded
                           + ' / ' 
-                          + PodcastCreateModel.episodesTotal
+                          + episodesTotal
                         ])
                       ]
                       : sr.trackCount + ' episodes'
                     ])
                   ]),
-                  PodcastCreateModel.feedUrl 
-                    && PodcastCreateModel.feedUrl != sr.feedUrl 
+                  feedUrl 
+                    && feedUrl != sr.feedUrl 
                     ? null 
                     : m('.w-12.flex.justify-center.items-center.bg-green-500', {
                     onclick() {
-                      PodcastCreateModel.addPodcast(sr.feedUrl).then(() => {
+                      addPodcast(sr.feedUrl).then(() => {
                         m.route.set('/podcasts')
                       })
                     }
                   }, [
-                    PodcastCreateModel.feedUrl == sr.feedUrl
+                    feedUrl == sr.feedUrl
                     ? m('i.fas.fa-spinner.fa-spin', {
                       disabled: true
                     })
@@ -88,12 +173,12 @@ function PodcastCreate() {
               type: 'text',
               placeholder: 'URL to podcast feed...',
               oninput(e) {
-                PodcastCreateModel.manualRssUrl = e.target.value
+                manualRssUrl = e.target.value
               }
             }),
             m('button.w-full.bg-green-500.mt-3.p-2.text-white', {
               onclick() {
-                PodcastCreateModel.addPodcast(PodcastCreateModel.manualRssUrl).then(() => {
+                addPodcast(manualRssUrl).then(() => {
                   m.route.set('/podcasts')
                 })
               }
