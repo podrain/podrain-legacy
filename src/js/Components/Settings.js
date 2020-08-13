cff2d3a0fbce7c3515260d2fc20819fc1d8216db
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-13 16:26:58 -0600

    convert Settings to JSX

--- src/js/Components/Settings.js
+++ src/js/Components/Settings.js
@@ -55,51 +55,75 @@ function Settings() {
 
   return {
     view() {
-      return m('.p-3', [
-        m('h1.text-white.text-xl.text-center', 'Settings'),
-        m('span.text-white', 'Proxy URL'),
-        m('input.w-full.p-1.mt-1', {
-          oninput(e) {
-            proxyURL = e.target.value
-          },
-          value: proxyURL
-        }),
-        m('button.w-full.bg-green-500.h-8.text-white', {
-          onclick() {
-            addProxyURL()
-            m.route.set('/')
+      return (
+        <div class="p-3">
+          <h1 class="text-white text-xl text-center">Settings</h1>
+          <span class="text-white">Proxy URL</span>
+          <input
+            class="w-full p-1 mt-1"
+            oninput={(e) => {
+              proxyURL = e.target.value
+            }}
+            value={proxyURL}
+          />
+          <button 
+            class="w-full bg-green-500 h-8 text-white"
+            onclick={() => {
+              addProxyURL()
+              m.route.set('/')
+            }}
+          >
+            <i class="fas fa-save mr-3"></i>
+            Save
+          </button>
+
+          <div class="mt-6">
+            <button 
+              class="bg-purple-600 p-1 text-white mr-1 w-full"
+              onclick={() => {
+                downloadBackup()
+              }}
+            >
+              <i class="fas fa-download mr-3"></i>
+              Download Backup
+            </button>
+          </div>
+
+          <h2 class="text-white mt-3">Restore backup</h2>
+          <input 
+            class="text-white mt-1"
+            type="file"
+            onchange={(e) => {
+              restoreFile = e.target.files[0]
+            }}
+          />
+
+          {
+            restoreFile &&
+              <button 
+                class="bg-orange-600 p-1 text-white mr-1 w-full mt-3"
+                disabled={restoring ? true : false}
+                onclick={() => {
+                  restoreBackup()
+                }}
+              >
+                {
+                  restoring ?
+                    <i class="fas fa-spinner fa-spin mr-3"></i>
+                  :
+                    <i class="fas fa-upload mr-3"></i>
+                }
+
+                {
+                  restoring ?
+                    restoreStatus
+                  : 
+                    'Restore backup'
+                }
+              </button>
           }
-        }, [
-          m('i.fas.fa-save.mr-3'),
-          'Save'
-        ]),
-        m('.mt-6', [
-          m('button.bg-purple-600.p-1.text-white.mr-1.w-full', {
-            onclick() {
-              downloadBackup()
-            }
-          }, [
-            m('i.fas.fa-download.mr-3'),
-            'Download backup'
-          ]),
-        ]),
-        m('h2.text-white.mt-3', 'Restore backup'),
-        m('input.text-white.mt-1', {
-          type: 'file',
-          onchange(e) {
-            restoreFile = e.target.files[0]
-          }
-        }),
-        restoreFile ? m('button.bg-orange-600.p-1.text-white.mr-1.w-full.mt-3', {
-          disabled: restoring ? true : false,
-          onclick() {
-            restoreBackup()
-          }
-        }, [
-          restoring ? m('.fas.fa-spinner.fa-spin.mr-3') : m('i.fas.fa-upload.mr-3'),
-          restoring ? restoreStatus : 'Restore backup'
-        ]) : null,
-      ])
+        </div>
+      )
     }
   }
 }
