d10bcb89163ce61f9507f25577dcce431dadaf51
Joe Sweeney <joe.sweeney224@gmail.com>
2020-08-12 22:14:10 -0600

    convert PodcastCreate to JSX

--- src/js/Components/PodcastCreate.js
+++ src/js/Components/PodcastCreate.js
@@ -91,101 +91,119 @@ function PodcastCreate() {
 
   return {
     view() {
-      return [
-        m('.p-3', [
-          m('h1.text-white.text-2xl', 'Add podcast'),
-          m('.flex.mt-3', [
-            m('button.flex-1.p-3', {
-              class: selectedTab == 'search' ? 'bg-orange-500 text-white' : 'bg-gray-900 text-white',
-              onclick() {
+      return (
+        <div class="p-3">
+          <h1 class="text-white text-2xl">Add Podcast</h1>
+          <div class="flex mt-3">
+            <button 
+              class={`flex-1 p-3 ${selectedTab == 'search' ? 'bg-orange-500 text-white' : 'bg-gray-900 text-white'}`}
+              onclick={() => {
                 selectedTab = 'search'
-              }
-            }, [
-              m('i.fas.fa-search.mr-3'),
-              'Search'
-            ]),
-            m('button.flex-1.p-3', {
-              class: selectedTab == 'rss' ? 'bg-orange-500 text-white' : 'bg-gray-900 text-white',
-              onclick() {
+              }}
+            >
+              <i class="fas fa-search mr-3"></i>
+              Search
+            </button>
+            <button
+              class={`flex-1 p-3 ${selectedTab == 'rss' ? 'bg-orange-500 text-white' : 'bg-gray-900 text-white'}`}
+              onclick={() => {
                 selectedTab = 'rss'
-              }
-            }, [
-              m('i.fas.fa-rss.mr-3'),
-              'RSS Feed'
-            ]),
-          ]),
-          selectedTab == 'search' ? [
-            m('input.w-full.mt-3.p-1', {
-              type: 'text',
-              placeholder: 'Podcast title to search...',
-              oninput(e) {
-                setSearch(e.target.value)
-              }
-            }),
-            searching 
-            ? m('h2.mt-3.text-white', 'Searching...')
-            : m('ul.mt-3', [
-              searchResults.map((sr, index) => {
-                return m('li.flex.text-white', {
-                  class: index > 0 ? 'mt-3' : '',
-                }, [
-                  m('.p-3.bg-gray-700.w-full.flex', [
-                    m('img.w-1/4', {
-                      src: sr.artworkUrl100
-                    }),
-                    m('.w-3/4.ml-3', [
-                      m('h3', sr.collectionName),
-                      feedUrl == sr.feedUrl
-                      ? [
-                        m('.italic.text-gray-200', [
-                          'Adding episodes: ',
-                          m('br'),
-                          episodesAdded
-                          + ' / ' 
-                          + episodesTotal
-                        ])
-                      ]
-                      : sr.trackCount + ' episodes'
-                    ])
-                  ]),
-                  feedUrl 
-                    && feedUrl != sr.feedUrl 
-                    ? null 
-                    : m('.w-12.flex.justify-center.items-center.bg-green-500', {
-                    onclick() {
-                      addPodcast(sr.feedUrl).then(() => {
-                        m.route.set('/podcasts')
-                      })
-                    }
-                  }, [
-                    feedUrl == sr.feedUrl
-                    ? m('i.fas.fa-spinner.fa-spin', {
-                      disabled: true
-                    })
-                    : m('i.fas.fa-plus')
-                  ])
-                ])
-              })
-            ])
-          ] : null,
-          selectedTab == 'rss' ? [
-            m('input.w-full.mt-3.p-1', {
-              type: 'text',
-              placeholder: 'URL to podcast feed...',
-              oninput(e) {
-                manualRssUrl = e.target.value
-              }
-            }),
-            m('button.w-full.bg-green-500.mt-3.p-2.text-white', {
-              onclick() {
-                addPodcast(manualRssUrl).then(() => {
-                  m.route.set('/podcasts')
-                })
-              }
-            }, 'Submit')
-          ] : null,
-        ])
-      ]
+              }}
+            >
+              <i class="fas fa-rss mr-3"></i>
+              RSS Feed
+            </button>
+          </div>
+
+          {
+            selectedTab == 'search' &&
+                <input 
+                  type="text"
+                  class="w-full mt-3 p-1"
+                  placeholder="Podcast title to search...!"
+                  oninput={(e) => {
+                    setSearch(e.target.value)
+                  }}
+                />
+          }
+
+          {
+            selectedTab == 'search' &&
+              (searching ? 
+                <h2 class="mt-3 text-white">Searching...</h2>
+              :
+                <ul class="mt-3">
+                  {searchResults.map((sr, index) => 
+                    <li class={`text-white flex ${index > 0 && 'mt-3'}`}>
+                      <div class="p-3 bg-gray-700 w-full flex">
+                        <img class="w-1/4" src={sr.artworkUrl100} />
+                        <div class="w-3/4 ml-3">
+                          <h3>{sr.collectionName}</h3>
+                          { feedUrl == sr.feedUrl ?
+                              <div class="italic text-gray-200">
+                                Adding episodes:<br />
+                                {episodesAdded + '/' + episodesTotal}
+                              </div>
+                            :
+                              sr.trackCount + ' episodes'
+                          }
+                        </div>
+                      </div>
+
+                      {
+                        feedUrl && feedUrl != sr.feedUrl ?
+                          null
+                        :
+                          <div 
+                            class="w-12 flex justify-center items-center bg-green-500"
+                            onclick={() => {
+                              addPodcast(sr.feedUrl).then(() => {
+                                m.route.set('/podcasts')
+                              })
+                            }}
+                          >
+                            {
+                              feedUrl == sr.feedUrl ?
+                                <i 
+                                  class="fas fa-spinner fa-spin"
+                                  disabled={true}
+                                ></i>
+                              :
+                                <i class="fas fa-plus"></i>
+                            }
+                          </div>
+                        }
+                    </li>
+                  )}
+                </ul>
+              )
+          }
+
+          {
+            selectedTab == 'rss' &&
+              <input 
+                type="text"
+                class="w-full mt-3 p-1" 
+                placeholder="URL to podcast feed..."
+                oninput={(e) => {
+                  manualRssUrl = e.target.value
+                }}
+              />
+          }
+
+          {
+            selectedTab == 'rss' &&
+              <button 
+                class="w-full bg-green-500 mt-3 p-2 text-white"
+                onclick={() => {
+                  addPodcast(manualRssUrl).then(() => {
+                    m.route.set('/podcasts')
+                  })
+                }}
+              >Submit</button>
+          }
+        </div>
+      )
     }
   }
 }
